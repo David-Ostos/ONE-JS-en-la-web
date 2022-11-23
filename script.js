933ffe3d70413ea194b52cea0e77ae979642d772
David-Ostos <davidostos2@gmail.com>
2022-11-23 15:20:56 -0400

    clase02 completada

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
-
-const btn = document.querySelector('[data-form-btn]');
-
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  const list = document.querySelector('[data-list]');
-  const task = document.createElement('li');
-  task.classList.add('card');
-  input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
-
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
-
-  task.appendChild(taskContent);
-  task.appendChild(deleteIcon());
-  list.appendChild(task);
-};
+const btn = document.querySelector("[data-form-btn]"); //con document.querySelector("") se llama un elemento del html 
+const input = document.querySelector("[data-form-input]");// y con "data-xx-xx" se hace llamado al objeto que previamente se utiliza en el html
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+const createTask = (evento)=>{ //para no tener que utilizar la palabra function se utiliza =>
+  evento.preventDefault();
+  console.log(input.value);
+  input.value = "";
+}
+
+console.log(btn);
+console.log(input.value);// .value es para poder sacar la informacion del objeto en este caso es de la etiqueta input
+
+btn.addEventListener("click",createTask);
+
