3f264fdd2c0a86e6bf09f3f3a997aae43077b1ec
David-Ostos <davidostos2@gmail.com>
2022-11-23 15:58:25 -0400

    clase3 completada

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const btn = document.querySelector("[data-form-btn]"); //con document.querySelector("") se llama un elemento del html 
-const input = document.querySelector("[data-form-input]");// y con "data-xx-xx" se hace llamado al objeto que previamente se utiliza en el html
 
 //Arrow functions o funciones anonimas
 const createTask = (evento)=>{ //para no tener que utilizar la palabra function se utiliza =>
   evento.preventDefault();
-  console.log(input.value);
+  const input = document.querySelector("[data-form-input]");// y con "data-xx-xx" se hace llamado al objeto que previamente se utiliza en el html
+  const value = input.value;// .value es para poder sacar la informacion del objeto en este caso es de la etiqueta input
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card")
   input.value = "";
+  const content = //aqui se utiliza el backticks "``" para agregar contenido html y se trae la div del html que queremos que se imprima
+    `<div> 
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content; // aqui con task.innerHTML estamos agregando el contenido al html en donde esta el data-task en este caso 
+
+    list.appendChild(task); //con appendChild se agrega un hijo a una etiqueta en el html.
+
+  console.log(content);
 }
 
 console.log(btn);
-console.log(input.value);// .value es para poder sacar la informacion del objeto en este caso es de la etiqueta input
-
-btn.addEventListener("click",createTask);// aqui se declara que hacer cuando el boton se acciona
+// aqui se declara que hacer cuando el boton se acciona
+btn.addEventListener("click",createTask);
 
