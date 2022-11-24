8910cb8559da48d717ed84304ab26441c3ab4640
David-Ostos <davidostos2@gmail.com>
2022-11-24 09:28:47 -0400

    clase4 completada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+( () => {
+
 const btn = document.querySelector("[data-form-btn]"); //con document.querySelector("") se llama un elemento del html 
 
 //Arrow functions o funciones anonimas
@@ -9,20 +11,44 @@ const createTask = (evento)=>{ //para no tener que utilizar la palabra function 
   const task = document.createElement("li");
   task.classList.add("card")
   input.value = "";
-  const content = //aqui se utiliza el backticks "``" para agregar contenido html y se trae la div del html que queremos que se imprima
-    `<div> 
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content; // aqui con task.innerHTML estamos agregando el contenido al html en donde esta el data-task en este caso 
+  const taskContent = document.createElement("div");
+  taskContent.appendChild(checkComplete());
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(titleTask);
+  /*taskContent.appendChild(deleteAll())*/
 
+    task.appendChild(taskContent)
     list.appendChild(task); //con appendChild se agrega un hijo a una etiqueta en el html.
-
-  console.log(content);
+    console.log(taskContent)
 }
 
-console.log(btn);
 // aqui se declara que hacer cuando el boton se acciona
 btn.addEventListener("click",createTask);
 
+const checkComplete = () => {
+  let i = document.createElement("i");
+  i.classList.add("far","fa-check-square","icon");
+  i.addEventListener("click", completeTask);
+  return i;
+
+}
+
+const completeTask = (event) => {
+  const element = event.target;
+  element.classList.toggle("fas");// el toggle es para verificar si tiene o no la clase si la tiene la quita y si no la tiene la coloca 
+  element.classList.toggle("completeIcon")
+  element.classList.toggle("far");
+}
+
+
+/*
+const deleteAll = () => {
+
+  let i = document.createElement("i");
+  i.classList.add("fas","fa-trash-alt","trashIcon","icon");
+  return i;
+  
+}*/
+}) ();
